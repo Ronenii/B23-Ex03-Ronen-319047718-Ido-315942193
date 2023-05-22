412b46ec275e3abc71a95941397aba9f68cce645
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-05-22 18:06:08 +0300

    Added value out of range exception logic

--- Ex03.GarageLogic/ValueOutOfRangeException.cs
+++ Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -1,13 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Ex03.GarageLogic
 {
-    class ValueOutOfRangeException: Exception
+    public class ValueOutOfRangeException : Exception
     {
+        private float m_MaxValue;
+        private float m_MinValue;
 
-        // TODO: Write exception logic
+        public float MaxValue
+        {
+            get
+            {
+                return m_MaxValue;
+            }
+        }
+
+        public float MinValue
+        {
+            get
+            {
+                return m_MinValue;
+            }
+        }
+
+        public ValueOutOfRangeException(Exception i_InnerException, float i_MaxValue, float i_MinValue) : 
+            base(string.Format("Value must be between {0} and {1}",i_MinValue,i_MaxValue), i_InnerException)
+        {
+            m_MaxValue = i_MaxValue;
+            m_MinValue = i_MaxValue;
+        }
+
+
     }
 }
